25066d24f9e3a6bd3fb91a74bf166597c1567dd5
Luiz Fernando <luyzfernando@gmail.com>
2023-08-10 13:33:32 -0300

    adicionado nova condição de tempo "Pancadas isoladas"

--- custom_components/cgesp/cge_scrape.py
+++ custom_components/cgesp/cge_scrape.py
@@ -55,7 +55,7 @@ def __de_para_ceu__(ceu: str):
         return "sunny"
     elif ceu == "Poucas nuvens":
         return "partlycloudy"
-    elif ceu == "Nublado com chuva" or ceu == "Chuvisco":
+    elif ceu == "Nublado com chuva" or ceu == "Chuvisco" or ceu == "Pancadas isoladas":
         return "rainy"
     elif ceu == "Nublado" or ceu == "Encoberto":
         return "cloudy"
